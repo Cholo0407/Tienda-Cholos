dff74061e18d74975df35e23c93e97ff1202f729
unknown <aless070704@gmail.com>
2025-03-20 15:35:44 -0600

    SaleDetails crud

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,10 @@
 import express from "express";
+import saleDetailsRoutes from "./src/routes/saleDetails.js"
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/SaleDetails", saleDetailsRoutes)
 
 export default app;
