ef90ca1d7d4aab84be7603ae1640b4f243a7b4ef
Angel <angelfan1637@gmail.com>
2025-04-29 01:02:45 -0600

    trabajando pagina de inicio

--- frontend/tienda-cholos/src/pages/Home.jsx
+++ frontend/tienda-cholos/src/pages/Home.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import "../../styles/Home.css";
+import Imagenes from "../components/Imagenes";
+import PorqueElegirnos from "../components/PorqueElegirnos"; 
+
 
 const Home = () => {
   return (
-    <div className="main-header">
-      <div className="header-content">
-        <h1>Cholo's</h1>
-        <p className="tagline">El estilo que pisa fuerte</p>
-        <p className="subtagline">Compra en nuestra tienda</p>
-        <button className="cta-button">Compra en nuestra tienda</button>
+    <div className="main-home">
+      <div className="main-header">
+        <div className="header-content">
+          <h1>Cholo's</h1>
+          <p className="tagline">El estilo que pisa fuerte</p>
+          <p className="subtagline">Compra en nuestra tienda</p>
+          <button className="cta-button">Compra en nuestra tienda</button>
+        </div>
+        <div className="imagenes-separado">
+          <Imagenes />
+        </div>
       </div>
+
+      <PorqueElegirnos /> {/* <-- ya fuera del header */}
     </div>
   );
 };
