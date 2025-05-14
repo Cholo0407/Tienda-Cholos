7d3961f39c248dc1c491c06ab1d0db1b9b2ee84d
unknown <aless070704@gmail.com>
2025-05-14 10:27:29 -0600

    cambios arreglados

--- frontend/privada/administrador-tienda/src/App.jsx
+++ frontend/privada/administrador-tienda/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import SideMenu from './components/SideMenu.jsx';
 import ShoeStore from './pages/Products.jsx';
+import Login from './pages/login.jsx'
+import Dashboard from './pages/dashboard.jsx'
 
 function AppContent() {
   const location = useLocation();
@@ -15,7 +17,7 @@ function AppContent() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/products" element={<div>Productos</div>} />
+          <Route path="/products" element={<ShoeStore />} />
           <Route path="/customers" element={<div>Clientes</div>} />
           <Route path="/admins" element={<div>Admins</div>} />
           <Route path="/models" element={<div>Modelos</div>} />
