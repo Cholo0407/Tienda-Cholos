c57321db4fa6ab4bba53981cd4d0dc4e1cd8f888
20230300@ricaldone.edu.sv <20230300@ricaldone.edu.sv>
2025-05-22 10:45:35 -0600

    routes private 80%

--- frontend/privada/administrador-tienda/src/App.jsx
+++ frontend/privada/administrador-tienda/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation, Navigate } from 'react-router-dom';
 import SideMenu from './components/SideMenu.jsx';
 import ShoeStore from './pages/Products.jsx';
 import Login from './pages/login.jsx';
@@ -14,31 +14,34 @@ import { AuthProvider } from './Context/AuthToken.jsx';
 
 function AppContent() {
   const location = useLocation();
-  const hiddenRoutes = ['/login', '/register', '/recovery', '/verifyCode', '/newPassword'];
+  const hiddenRoutes = ['/login', '/recovery', '/verifyCode', '/newPassword'];
   const shouldShowMenu = !hiddenRoutes.includes(location.pathname);
 
   return (
     <div className="flex">
       {shouldShowMenu && <SideMenu />}
       <div className="flex-1">
-        <Routes>
-          {/* Rutas públicas */}
-          <Route path="/login" element={<Login />} />
-          <Route path="/recovery" element={<Recovery />} />
-          <Route path="/verifyCode" element={<VerifyCode />} />
-          <Route path="/newPassword" element={<NewPassword />} />
+      <Routes>
+  {/* Redirección desde "/" a "/dashboard" */}
+  <Route path="/" element={<Navigate to="/login" />} />
 
-          {/* Rutas protegidas */}
-          <Route element={<PrivateRoute />}>
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/products" element={<ShoeStore />} />
-            <Route path="/products/AgregarZapato" element={<ProductForm />} />
-            <Route path="/customers" element={<Clientes />} />
-            <Route path="/admins" element={<div>Admins</div>} />
-            <Route path="/models" element={<div>Modelos</div>} />
-            <Route path="/register" element={<div>Register</div>} />
-          </Route>
-        </Routes>
+  {/* Rutas públicas */}
+  <Route path="/login" element={<Login />} />
+  <Route path="/recovery" element={<Recovery />} />
+  <Route path="/verifyCode" element={<VerifyCode />} />
+  <Route path="/newPassword" element={<NewPassword />} />
+
+  {/* Rutas protegidas */}
+  <Route element={<PrivateRoute />}>
+    <Route path="/dashboard" element={<Dashboard />} />
+    <Route path="/products" element={<ShoeStore />} />
+    <Route path="/products/AgregarZapato" element={<ProductForm />} />
+    <Route path="/customers" element={<Clientes />} />
+    <Route path="/admins" element={<div>Admins</div>} />
+    <Route path="/models" element={<div>Modelos</div>} />
+    <Route path="/register" element={<div>Register</div>} />
+  </Route>
+</Routes> 
       </div>
     </div>
   );
